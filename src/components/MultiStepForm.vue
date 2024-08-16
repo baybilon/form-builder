9843da9c8ea3f5bde3e508d03e1fc6cbbeaf3b2c
baybilon <baybilon_iqbal@hotmail.com>
2024-08-16 08:06:06 +0700

    Hello there

--- src/components/MultiStepForm.vue
+++ src/components/MultiStepForm.vue
@@ -1,49 +1,65 @@
 <template>
-    <div>
-      <div v-if="currentStep">
+  <div class="relative">
+    <div class="absolute w-full grid place-items-center h-screen">
+    <div class="bg-neutral-100 rounded-xl mx-0 ">
+      <div class="p-10" v-if="currentStep">
         <h2 class="text-2xl font-bold">{{ currentStep.title }}</h2>
         <p class="text-gray-600">{{ currentStep.description }}</p>
         <form @submit.prevent="nextStep">
           <div v-for="field in currentStep.fields" :key="field.label" class="my-4">
             <label :for="field.label" class="block text-gray-700">{{ field.label }}</label>
+
             <input v-if="field.type === 'textfield'" 
-                   :id="field.label"
-                   v-model="formData[field.label]"
-                   :placeholder="field.placeholder" 
-                   :required="field.required" 
-                   class="w-full px-4 py-2 border rounded-md" />
+                  :id="field.label"
+                  v-model="formData[field.label]"
+                  :placeholder="field.placeholder" 
+                  :required="field.required" 
+                  class="w-full px-4 py-2 border rounded-md" />
+
             <textarea v-if="field.type === 'textarea'"
                       :id="field.label"
                       v-model="formData[field.label]"
                       :placeholder="field.placeholder"
                       :required="field.required"
                       class="w-full px-4 py-2 border rounded-md"></textarea>
+
             <div v-if="field.type === 'radio'">
               <label v-for="option in field.options" :key="option.value" class="inline-flex items-center mt-2">
                 <input type="radio"
-                       :name="field.label"
-                       :value="option.value"
-                       v-model="formData[field.label]"
-                       :required="field.required"
-                       class="form-radio h-4 w-4" />
-                <span class="ml-2">{{ option.label }}</span>
+                      :name="field.label"
+                      :value="option.value"
+                      v-model="formData[field.label]"
+                      :required="field.required"
+                      class="form-radio h-4 w-4" />
+                <span class="ml-1 mr-4">{{ option.label }}</span>
               </label>
             </div>
+
             <div v-if="field.type === 'autocomplete'">
               <select v-model="formData[field.label]" :id="field.label" class="w-full px-4 py-2 border rounded-md">
                 <option v-for="option in field.options" :key="option" :value="option">{{ option }}</option>
               </select>
             </div>
+
           </div>
-          <button type="submit" class="px-6 py-2 bg-blue-600 text-white rounded-md">Next</button>
+
+          <button type="button" @click="prevStep" class="px-6 py-2 bg-gray-300 text-black rounded-md mr-2" v-if="currentStepIndex > 0">
+            Back
+          </button>
+
+          <button type="submit" class="px-6 py-2 bg-cyan-500 text-white rounded-md">
+            Next
+          </button>
         </form>
       </div>
-      <div v-else>
-        <p>Form completed! Here is your data:</p>
+      <div v-else class="p-10">
+        <p>Here is your data: </p>
         <pre>{{ formData }}</pre>
       </div>
     </div>
-  </template>
+  </div>
+  </div>
+</template>
   
   <script>
   import formConfig from '../data/response.json';
@@ -57,20 +73,25 @@
       };
     },
     computed: {
-      currentStep() {
-        return this.formConfig[this.currentStepIndex];
+    currentStep() {
+      return this.formConfig[this.currentStepIndex];
+    }
+  },
+  methods: {
+    nextStep() {
+      if (this.currentStepIndex < this.formConfig.length - 1) {
+        this.currentStepIndex++;
+      } else {
+        this.currentStepIndex = null;
       }
     },
-    methods: {
-      nextStep() {
-        if (this.currentStepIndex < this.formConfig.length - 1) {
-          this.currentStepIndex++;
-        } else {
-          this.currentStepIndex = null; // Form completed
-        }
+    prevStep() {
+      if (this.currentStepIndex > 0) {
+        this.currentStepIndex--;
       }
     }
-  };
+  }
+};
   </script>
   
   <style scoped>
